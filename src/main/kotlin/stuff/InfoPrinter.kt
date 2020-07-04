dc1fcbd41021b90df8060026db2849105aee4a1d
knokko <knokogator@hotmail.com>
2020-07-04 14:29:05 +0200

    Add GLFW debugging and fix my confusion between init hints and window hints

--- src/main/kotlin/stuff/InfoPrinter.kt
+++ src/main/kotlin/stuff/InfoPrinter.kt
@@ -6,11 +6,12 @@ import org.lwjgl.opengl.GL11
 import org.lwjgl.opengl.GL30.*
 import org.lwjgl.openvr.*
 import org.lwjgl.openvr.VR.*
-import org.lwjgl.openvr.VRSystem.VRSystem_GetRecommendedRenderTargetSize
-import org.lwjgl.openvr.VRSystem.VRSystem_GetTrackedDeviceClass
+import org.lwjgl.openvr.VRCompositor.*
+import org.lwjgl.openvr.VRSystem.*
 import org.lwjgl.system.MemoryStack.stackPush
 import org.lwjgl.system.MemoryUtil
 import org.lwjgl.system.MemoryUtil.NULL
+import org.lwjgl.system.MemoryUtil.memUTF8
 import java.awt.Color
 
 fun main() {
@@ -20,9 +21,12 @@ fun main() {
 
     // Apparently, creating a cross-platform OpenGL context without window is very hard
     // So the work-around is to just create an invisible window
+    glfwSetErrorCallback { errorCode, description ->
+        println("GLFW error $errorCode: ${memUTF8(description)}")
+    }
+    glfwInitHint(GLFW_COCOA_MENUBAR, GLFW_FALSE)
     glfwInit()
     glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE)
-    glfwWindowHint(GLFW_COCOA_MENUBAR, GLFW_FALSE)
 
     val windowHandle = glfwCreateWindow(1, 1, "Should be invisible", NULL, NULL)
     glfwMakeContextCurrent(windowHandle)
@@ -32,7 +36,7 @@ fun main() {
     stackPush().use{stack ->
 
         val pError = stack.mallocInt(1)
-        val token = VR_InitInternal(pError, 0)
+        val token = VR_InitInternal(pError, EVRApplicationType_VRApplication_Scene)
 
         println("Error code for init is ${pError[0]}")
         println("Token is $token")
@@ -41,9 +45,14 @@ fun main() {
             OpenVR.create(token)
 
             println("Device classes:")
-            for (deviceIndex in 0 until k_unMaxTrackedDeviceCount)
-                println(VRSystem_GetTrackedDeviceClass(deviceIndex))
-            println()
+            for (deviceIndex in 0 until k_unMaxTrackedDeviceCount) {
+                val deviceClass = VRSystem_GetTrackedDeviceClass(deviceIndex)
+                if (deviceClass != 0) {
+                    val connected = VRSystem_IsTrackedDeviceConnected(deviceIndex)
+                    println("Device class is $deviceClass and connected is $connected")
+                }
+            }
+            println("These were all device classes")
 
             val pWidth = stack.mallocInt(1)
             val pHeight = stack.mallocInt(1)
@@ -77,16 +86,21 @@ fun main() {
             leftTexture.handle(leftFramebuffer.textureHandle.toLong())
 
             // Stop after 20 seconds
-            val endTime = System.currentTimeMillis() + 20_000
+            val endTime = System.currentTimeMillis() + 2_000
 
             val poses = TrackedDevicePose.mallocStack(k_unMaxTrackedDeviceCount, stack)
+            poses.eTrackingResult(123)
             println("Start while loop")
             while (System.currentTimeMillis() < endTime) {
-                VRCompositor.VRCompositor_WaitGetPoses(poses, null)
-                VRCompositor.VRCompositor_Submit(EVREye_Eye_Left, leftTexture, null, EVRSubmitFlags_Submit_TextureWithDepth)
-                VRCompositor.VRCompositor_PostPresentHandoff()
-                print("s")
+                println("Poses: ${VRCompositor_WaitGetPoses(poses, null)}")
+                println("Submit left: ${VRCompositor_Submit(EVREye_Eye_Left, leftTexture, null, EVRSubmitFlags_Submit_TextureWithDepth)}")
+                // TODO Use a right texture as well
+                VRCompositor_Submit(EVREye_Eye_Right, leftTexture, null, EVRSubmitFlags_Submit_TextureWithDepth)
+                VRCompositor_PostPresentHandoff()
             }
+
+            println("Poses info: connected is ${poses.bDeviceIsConnected()} and valid is ${poses.bPoseIsValid()}")
+            println("Poses info: tracking result is ${poses.eTrackingResult()} and velocity is ${poses.vAngularVelocity()}")
 
             println("End while loop")
 
@@ -102,6 +116,8 @@ fun main() {
         VR_ShutdownInternal()
         println("Shutdown successfully")
     }
+
+    println("OpenGL error:" + glGetError())
 
     GL.destroy()
     glfwDestroyWindow(windowHandle)
